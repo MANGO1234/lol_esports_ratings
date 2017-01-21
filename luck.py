1584a468fcf5c652f36ee260d57759d369744642
De Li <de_li@live.com>
2017-01-21 13:31:04 -0800

    PEP8 Autoformat

--- luck.py
+++ luck.py
@@ -36,36 +36,36 @@ def getTeam(n):
 
 def updateTeam(n, rs):
     getTeam(n)
-    teams[n]["game"]+=1
-    if rs==1:
-        teams[n]["wins"]+=1
+    teams[n]["game"] += 1
+    if rs == 1:
+        teams[n]["wins"] += 1
 
 
 for m in ms:
     updateTeam(m["t1"], m["result"])
-    updateTeam(m["t2"], 1-m["result"])
+    updateTeam(m["t2"], 1 - m["result"])
 
 i = 1
 print("{:3}  {:25}  {:5}  {:5}  {:5}".format("", "Team", "Games", "Wins", "%"))
 tss = sorted(teams.values(), key=lambda x: x["game"], reverse=True)
 for p in tss:
-    print("{:3d}  {:25.25}  {:<5d}  {:<5d}  {:<8.4f}".format(i, p["name"], p["game"], p["wins"], p["wins"]/p["game"]))
+    print("{:3d}  {:25.25}  {:<5d}  {:<5d}  {:<8.4f}".format(i, p["name"], p["game"], p["wins"], p["wins"] / p["game"]))
     i += 1
 print()
 
-s=0
-m=0
-i=0
-c=0
+s = 0
+m = 0
+i = 0
+c = 0
 for t in tss:
-    i+=1
-    m+=t["game"]
-    c+=(t["wins"]/t["game"])
-c/=i
+    i += 1
+    m += t["game"]
+    c += (t["wins"] / t["game"])
+c /= i
 for t in tss:
-    s+=((t["wins"]/t["game"])-c)*((t["wins"]/t["game"])-c)
-m/=2
+    s += ((t["wins"] / t["game"]) - c) * ((t["wins"] / t["game"]) - c)
+m /= 2
 print(c)
-print((0.25/m))
-print((s/i))
-print((0.25/m)/(s/i))
+print((0.25 / m))
+print((s / i))
+print((0.25 / m) / (s / i))
