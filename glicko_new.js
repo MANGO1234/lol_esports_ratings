d9b8709ba20bea10caed10c93c42c6862447816c
De Li <de_li@live.com>
2017-01-22 22:21:20 -0800

    added short names

--- glicko_new.js
+++ glicko_new.js
@@ -4,6 +4,7 @@ var glicko2 = require('glicko2');
 var fs = require('fs');
 var printf = require('printf');
 var config = require('./matches/leagues.json');
+var namesTranslate = require('./matches/names.json');
 
 
 function getMatches(key) {
@@ -42,14 +43,16 @@ function calculateModel(matches, type) {
     var players = {};
 
     function getPlayer(name) {
-        if (!players[name]) {
-            players[name] = {
-                name: name,
+        shortName = namesTranslate[name] || name;
+        if (!players[shortName]) {
+            players[shortName] = {
+                name: shortName,
+                fullName: name,
                 player: ranking.makePlayer(),
                 history: {}
             };
         }
-        return players[name].player;
+        return players[shortName].player;
     }
 
     if (type === 'ALL') {
@@ -104,6 +107,7 @@ function calculateModel(matches, type) {
             ranking.updateRatings(period.matches.map((match) => [getPlayer(match.t1), getPlayer(match.t2), match.result]));
             period.ratings = _.mapValues(players, function(player) {
                 return {
+                    name: player.name,
                     rating: player.player.getRating(),
                     rd: player.player.getRd(),
                     vol: player.player.getVol()
@@ -151,9 +155,9 @@ getMatches(key).then(function(matches) {
     stream.once('open', function() {
         write('******** ' + model.matches.name + ' ********');
         write('**** Current Ratings ****');
-        write(printf('%-8s %-25s %-8s %-8s %-8s %-8s %-8s', 'Ranking', 'Team', 'Rating', 'RD', 'Min', 'Max', 'Vol'));
+        write(printf('%-8s %-30s %-8s %-8s %-8s %-8s %-8s', 'Ranking', 'Team', 'Rating', 'RD', 'Min', 'Max', 'Vol'));
         playersA.forEach(function(v, i) {
-            write(printf('%-8d %-25s %-8.1f %-8.1f %-8.1f %-8.1f %-8.5f', i + 1, v.name, v.player.getRating(), v.player.getRd() * 2,
+            write(printf('%-8d %-30s %-8.1f %-8.1f %-8.1f %-8.1f %-8.5f', i + 1, v.fullName.substring(0, 30), v.player.getRating(), v.player.getRd() * 2,
                 v.player.getRating() - v.player.getRd() * 2, v.player.getRating() + v.player.getRd() * 2, v.player.getVol()));
         });
         write();
@@ -163,28 +167,30 @@ getMatches(key).then(function(matches) {
         write('Range of Ratings: ' + (playersA[0].player.getRating() - playersA[playersA.length - 1].player.getRating()));
         write();
 
-        write('**** Ratings by Period ****');
-        formatS = '%-25s %-10s %-10s ' + _.repeat('%-25s ', playersA.length);
-        write(_.spread(_.partial(printf, formatS, 'Period', 'Start', 'End'))(playersA.map((p) => p.name)));
-        formatS = '%-25s %-10s %-10s ' + _.repeat('%-25.1f ', playersA.length);
-        model.ratingPeriods.forEach(function(period, i) {
-            write(_.spread(_.partial(printf, formatS, i + 1, period.startDate, period.endDate))(playersA.map((p) => period.ratings[p.name].rating)));
-        });
-        write();
+        if (model.ratingPeriods) {
+            write('**** Ratings by Period ****');
+            formatS = '%-8s %-12s %-12s ' + _.repeat('%-8s ', playersA.length);
+            write(_.spread(_.partial(printf, formatS, 'Period', 'Start', 'End'))(playersA.map((p) => p.name)));
+            formatS = '%-8s %-12s %-12s ' + _.repeat('%-8.1f ', playersA.length);
+            model.ratingPeriods.forEach(function(period, i) {
+                write(_.spread(_.partial(printf, formatS, i + 1, period.startDate, period.endDate))(playersA.map((p) => period.ratings[p.name].rating)));
+            });
+            write();
+        }
 
         write('**** Estimated Win Rates (BO1) ****');
-        formatS = '%-25s' + _.repeat('%-25s ', playersA.length);
+        formatS = '%-6s' + _.repeat('%-8s ', playersA.length);
         write(_.spread(_.partial(printf, formatS, ''))(playersA.map((p) => p.name)));
-        formatS = '%-25s' + _.repeat('%-25.2f ', playersA.length);
+        formatS = '%-6s' + _.repeat('%-8.2f ', playersA.length);
         playersA.forEach(function(p1) {
             write(_.spread(_.partial(printf, formatS, p1.name))(playersA.map((p2) => ratingToWinRate(p1, p2) * 100)));
         });
         write();
 
         write('**** Estimated Win Rates (BO3) ****');
-        formatS = '%-25s' + _.repeat('%-25s ', playersA.length);
+        formatS = '%-6s' + _.repeat('%-8s ', playersA.length);
         write(_.spread(_.partial(printf, formatS, ''))(playersA.map((p) => p.name)));
-        formatS = '%-25s' + _.repeat('%-25.2f ', playersA.length);
+        formatS = '%-6s' + _.repeat('%-8.2f ', playersA.length);
         playersA.forEach(function(p1) {
             write(_.spread(_.partial(printf, formatS, p1.name))(playersA.map((p2) => {
                 var p = ratingToWinRate(p1, p2);
