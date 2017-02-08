0f2f8cc28d4e21db2795f8d0985c9c62b71b1351
MANGO12345 <814583+MANGO1234@users.noreply.github.com>
2017-02-08 00:42:17 -0800

    more

--- modelPerf.js
+++ modelPerf.js
@@ -21,6 +21,7 @@ function runModel(scoring, model, matches, n) {
     return scoring;
 }
 
+// scoring
 function RMSE() {
     return {
         score: 0,
@@ -49,6 +50,7 @@ function BrierScore() {
     };
 }
 
+// models
 function coin_flip(data) {
     return 0.5;
 }
@@ -68,11 +70,70 @@ function glicko_all(data) {
     return ratingToWinRate(p1, p2);
 }
 
-s.getMatches("lck16br").then(function(matches) {
-    console.log(runModel(BrierScore(), coin_flip, matches, 100).getScore());
-    console.log(runModel(BrierScore(), perfect, matches, 100).getScore());
-    console.log(runModel(BrierScore(), t1Wins, matches, 100).getScore());
-    console.log(runModel(BrierScore(), glicko_all, matches, 100).getScore());
+function glicko_single(data) {
+    var model = calculateModel(data.matches, "SINGLE");
+    var p1 = model.getPlayer(data.predict.t1);
+    var p2 = model.getPlayer(data.predict.t2);
+    return ratingToWinRate(p1, p2);
+}
+
+function glicko_week(data) {
+    var model = calculateModel(data.matches);
+    var p1 = model.getPlayer(data.predict.t1);
+    var p2 = model.getPlayer(data.predict.t2);
+    return ratingToWinRate(p1, p2);
+}
+
+// doesn't change ratings during the period
+function glicko_week2(data) {
+    data.matches.push(data.predict);
+    var model = calculateModel(data.matches);
+    var tmp = model.ratingPeriods.map((tmp) => tmp.matches);
+    var k = [];
+    for (let i = 0; i < tmp.length - 1; i++) {
+        k = k.concat(tmp[i]);
+    }
+    model = calculateModel(k);
+    var p1 = model.getPlayer(data.predict.t1);
+    var p2 = model.getPlayer(data.predict.t2);
+    return ratingToWinRate(p1, p2);
+}
+
+// analysis
+Promise.resolve().then(function() {
+    return s.getMatches("lck16ar");
+}).then(function(matches) {
+    console.log("lck");
+    var start = Math.round(matches.length / 2);
+    console.log(runModel(BrierScore(), coin_flip, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_all, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_single, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_week, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_week2, matches, start).getScore());
+}).catch(function(e) {
+    console.log(e);
+}).then(function() {
+    return s.getMatches("na16br");
+}).then(function(matches) {
+    console.log("na");
+    var start = Math.round(matches.length / 2);
+    console.log(runModel(BrierScore(), coin_flip, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_all, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_single, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_week, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_week2, matches, start).getScore());
+}).catch(function(e) {
+    console.log(e);
+}).then(function() {
+    return s.getMatches("eu16br");
+}).then(function(matches) {
+    console.log("eu");
+    var start = Math.round(matches.length / 2);
+    console.log(runModel(BrierScore(), coin_flip, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_all, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_single, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_week, matches, start).getScore());
+    console.log(runModel(BrierScore(), glicko_week2, matches, start).getScore());
 }).catch(function(e) {
     console.log(e);
 });
